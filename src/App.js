d86a7b2d331ffaa73d83f5b9c3af3652ceab6c29
dangerstephen <dangersteve1@gmail.com>
2017-05-08 14:33:02 -0700

    hard coded one review and did some css to get the basic look I plan to use. Now will start pulling data from the api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    <div className="card">
+     <div className="wrapper">
+      <div className="date">
+       <span className="day">Day</span>
+       <span className="month">Month</span>
+       <span className="year">Year</span>
+     </div>
+     <div className="data">
+        <div className="content">
+          <h1 className="title">Title</h1>
+           <span className="reviwer">Reviwer</span>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       </div>
+    </div>
+  </div>
     );
   }
 }
